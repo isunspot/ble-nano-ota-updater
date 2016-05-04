bc578df05cc9947412456d6c4aad434f1b010430
AntonBazhal <am.bazhal@gmail.com>
2016-05-04 17:27:25 -0400

    Removed some annoying autogenerated code

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -8,8 +8,7 @@ angular
         // gulp environment: injects environment vars
         ENV: {
             /*inject-env*/
-            'SERVER_URL': 'https://DEVSERVER/api',
-    'SOME_OTHER_URL': '/proxy'
+            'SERVER_URL': 'https://DEVSERVER/api'
             /*endinject*/
         },
 
